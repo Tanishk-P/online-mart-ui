241809bc113882274d549879f69884762b494d68
Tanishk P <tanisk@maxscriptstechnologies.com>
2023-04-26 18:09:10 +0530

    Added a home page

--- src/utls/PageRoutes.ts
+++ src/utls/PageRoutes.ts
@@ -1,9 +1,11 @@
 interface IPageRoutes {
     login: string,
     signUp: string,
+    home: string,
 }
 
 export const PageRoutes: IPageRoutes = {
     login: "/",
     signUp: "/signUp",
+    home: "/home",
 } 
