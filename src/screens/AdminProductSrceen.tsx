b18e3a5db59f548c8047576369ae824f1fb65db1
tanishkparadkar <tanishkparadkar@gmail.com>
2023-05-14 20:07:59 +0530

    Added the add product page, and made new admin routes

--- src/screens/AdminProductSrceen.tsx
+++ src/screens/AdminProductSrceen.tsx
@@ -1,19 +1,17 @@
-import { Carousel, Col, Divider, Row, Typography } from 'antd';
 import { Content, Header } from 'antd/es/layout/layout';
 import React from 'react'
 import { colors } from '../utls/Color';
+import { Col, Divider, Row, Typography } from 'antd';
 import * as labelConst from '../utls/Labels';
-import Logo from '../components/Logo';
-import CommonButton from '../components/CommonButton';
 import CommonHeader from '../components/CommonHeader';
+import CommonButton from '../components/CommonButton';
 import { PageRoutes } from '../utls/PageRoutes';
 import { useNavigate } from 'react-router-dom';
 
-function AdminScreen() {
-    const navigate = useNavigate();
-
+function AdminProductInfo() {
+  const navigate = useNavigate();
   return (
-    <>
+    <>      
         <Content className='admin-screen-content' style={{ backgroundColor: "#f0f0f0", height: "90vh" }}>
             <Row >
                 <Col>
@@ -26,4 +24,4 @@ function AdminScreen() {
   )
 }
 
-export default AdminScreen;
+export default AdminProductInfo;
